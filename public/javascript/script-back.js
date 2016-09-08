1fe3b64e9f88faa978af7b0cc4e94f05505280f3
jerossh <jerosshen@gmail.com>
2016-09-08 23:41:55 +0800

    Event Delegation

--- public/javascript/script-back.js
+++ public/javascript/script-back.js
@@ -43,25 +43,53 @@
     }
     // 函数节流
     function throtle(method, context) {
-      clearInterval(method.tid)
-      method.tid = setInterval(function(){
+      setTimeout(method.tid)
+      method.tid = setTimeout(function(){
         method.call(context)
-      }, 100)
+      }, 200)
     }
     $(window).resize(function(){
       throtle(changeNav)
     })
+    // 加载的时候执行一次
+    changeNav()
     // 导航部分结束
 
 
     // 表格列表功能
     // 提交功能
+
     $('button[type="submit"]').on('submit', function() {
       $(this).prop("disabled", true)
     })
 
+    // 表格内事件委托
+    var arr = ['del', 'change', 'view_blog']
+    // 这里开始委托
+    $('table').on('click', 'button', function(e) {
+      var that = this
+      arr.forEach(function(val){
+        console.log(val);
+        if($(that).hasClass(val)) {
+          console.log(val);
+          switch (val) {
+            case 'del':
+              del(e)
+              break;
+            case 'change':
+              change(e)
+              break;
+            case 'view_blog':
+              view_blog(e)
+              break;
+
+          }
+        }
+      })
+      $(this).hasClass('.change')
+    })
     // 删除功能
-    $('.del').click(function(e) {
+    function del(e){
       var target = $(e.target)
       var id = target.data('id')
       var item = $('.item-id-' + id.split('.')[0])
@@ -77,22 +105,64 @@
             }
         }
       })
-    })
+    }
 
-    // 修改功能
-    $('.change').click(function(e) {
+    function change(e){
       var target = $(e.target)
       var id = target.data('id')
       location.search = "?id=" + id
-    })
+    }
 
-    // 查看博客
-    $('.view_blog').click(function(e) {
+    function view_blog(e) {
       var target = $(e.target)
       var id = target.data('id')
       var url = "/blog/article" + "?id=" + id
       window.open(url)
-    })
+    }
+    // 表格内事件委托结束
+
+
+
+    //   // 删除功能
+    // $('.del').click(function(e) {
+    //   var target = $(e.target)
+    //   var id = target.data('id')
+    //   var item = $('.item-id-' + id.split('.')[0])
+    //   var delUrl = pathname + '?id='
+    //
+    //   $.ajax({
+    //     type: 'DELETE',
+    //     url: delUrl + id
+    //   }).done(function(result){
+    //     if (result.success == 1) {
+    //         if(item.length > 0) {
+    //           item.remove()
+    //         }
+    //     }
+    //   })
+    // })
+    //
+    //
+    //
+    //
+    // // 修改功能
+    // $('.change').click(function(e) {
+    //   var target = $(e.target)
+    //   var id = target.data('id')
+    //   location.search = "?id=" + id
+    // })
+    //
+    // // 查看博客
+    // $('.view_blog').click(function(e) {
+    //   var target = $(e.target)
+    //   var id = target.data('id')
+    //   var url = "/blog/article" + "?id=" + id
+    //   window.open(url)
+    // })
+
+
+
+
 
     // 博客类别点击显示，用 css 兄弟选择服解决了
     // $('.mt-checkbox>span').click(function(){
